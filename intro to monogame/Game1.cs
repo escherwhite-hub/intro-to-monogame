b6d05bb5235664db0ca09661225b529039b5693f
362707242 <362707242@2260I-R31-001>
2026-04-07 13:07:40 -0400

    Add project files.

---
+++ intro to monogame/Game1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace intro_to_monogame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+        Texture2D turtleTexture;
+        Texture2D thwompTexture;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            window = new Rectangle(0, 0, 800, 600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            this.Window.Title = "Adding Content";
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            turtleTexture = Content.Load<Texture2D>("turtle");
+            thwompTexture = Content.Load<Texture2D>("thwomp");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.MonoGameOrange);
+            _spriteBatch.Begin();
+            
+            _spriteBatch.Draw(turtleTexture, new Vector2(67, 175), Color.White); 
+            _spriteBatch.Draw(turtleTexture, new Vector2(300, 120), Color.Black);
+            _spriteBatch.Draw(turtleTexture, new Vector2(533, 67), Color.White);
+            _spriteBatch.Draw(thwompTexture, new Vector2(128, 77), Color.Wheat);
+            _spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
